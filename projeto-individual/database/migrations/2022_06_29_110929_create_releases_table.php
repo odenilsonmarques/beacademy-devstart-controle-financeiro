5a07314d39cb5f13dfdcc5951c2d7e4a468d961d
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-06-29 09:08:31 -0300

    configuracao do arquivo env,e geracao das migration

---
+++ projeto-individual/database/migrations/2022_06_29_110929_create_releases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('releases', function (Blueprint $table) {
+            $table->id();
+            $table->string('release_type');
+            $table->string('person');
+            $table->string('description');
+            $table->decimal('amount');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('releases');
+    }
+};
