8adefc532cf98a809b6382a566997295172ad320
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-07-23 21:24:55 -0300

    implementacao da logica de exclusao

--- projeto-individual/app/Http/Controllers/ReleaseController.php
+++ projeto-individual/app/Http/Controllers/ReleaseController.php
@@ -34,4 +34,10 @@ class ReleaseController extends Controller
         Release::create($data);
         return redirect()->route('lancamentos.list');
     }
+
+    public function destroy($id)
+    {
+        Release::find($id)->delete();
+        return redirect()->route('lancamentos.list');
+    }
 }
