0a27f1ff0518ccda5e4045152e786f5f1d6cb0d4
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-08-23 15:29:12 -0300

    organizacao no formulario de edicao e implementacao de icones na tabela do dashboard

--- projeto-individual/resources/views/dashboard/dashboard.blade.php
+++ projeto-individual/resources/views/dashboard/dashboard.blade.php
@@ -89,14 +89,24 @@
             @foreach($releases as $release)
               @if($release->release_type == 'DESPESA')
                 <tr class="lineRed">
-                  <td>{{$release->release_type}}</td>
+                  <td>
+                    {{$release->release_type}} 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-down-circle mb-1" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                    </svg>
+                  </td>
                   <td>{{$release->person}}</td>
                   <td>R$ {{number_format($release->amount, 2, ',', '.')}}</td>
                   <td>{{date('d/m/Y',strtotime($release->due_date))}}</td>
                 </tr>
               @else
                 <tr class="lineDark">
-                  <td>{{$release->release_type}}</td>
+                  <td>
+                    {{$release->release_type}}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-up-circle mb-1" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                    </svg>
+                  </td>
                   <td>{{$release->person}}</td>
                   <td>R$ {{number_format($release->amount, 2, ',', '.')}}</td>
                   <td>{{date('d/m/Y',strtotime($release->due_date))}}</td>
--- projeto-individual/resources/views/releases/edit.blade.php
+++ projeto-individual/resources/views/releases/edit.blade.php
@@ -3,8 +3,8 @@
 
 @section('content')
     <div class="container">
-        <div class="row mt-4">
-            <div class="col-sm-6 offset-md-3">
+        <div class="row">
+            <div class="col-sm-6 mt-5">
                 @if($errors->any())
                     <div class="alert alert-danger">
                         <ul>
@@ -16,55 +16,63 @@
                         </ul>
                     </div>
                 @endif
+                <img src="{{asset('assets/img/edit.svg')}}" alt="logo"  width="" height="" class="d-inline-block align-text-center mb-2">
+            </div>
+            <div class="col-sm-6 mt-5">
+                <div class="card">
+                    <div class="card-header">Alteração de Lançamento</div>
+                    <div class="card-body">
+                        <form action="{{route('release.editAction',$data->id)}}" method="POST">
+                            @csrf<!--csrf toquem de segurnça padrao do laravel para envio de requisao-->
+                            @method('PUT')<!--essa directiva é usada por causa do verdo put declarado na rota-->
+            
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <label for="release_type">Tipo de Lançamento</label>
+                                    <!--para carregar esse campo preenchido, verifica-se se existe um pet para edicao($data) se existir e for igual ao tipo de pet selecionado é carregado o campo preenchido com valor escolhido-->
+                                    <select name="release_type" id="" class="form-select">
+                                        <option value="">----Selecione----</option>
+                                        @foreach($typeReleases as $typeRelease)
+                                            <option value="{{$typeRelease}}" @if( isset($data) && $data -> release_type == $typeRelease) selected @endif>{{$typeRelease}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 mt-3">
+                                    <label for="person">Pessoa</label>
+                                    <input type="text" name="person" id="person" value="{{$data->person}}" class="form-control"  maxlength="30" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 mt-3">
+                                    <label for="amount">Valor</label>
+                                    <input type="text" name="amount" id="amount" value="{{$data->amount}}" class="form-control" onkeyup="formatCoin();"  placeholder="R$" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 mt-3">
+                                    <label for="due_date">Data</label>
+                                    <input type="date" name="due_date" id="due_date" value="{{$data->due_date}}" class="form-control" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 mt-3">
+                                    <label for="description">Descrição</label>
+                                    <input type="text" name="description" id="description" value="{{$data->release_type}}" class="form-control"  maxlegth="30" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 mt-4">
+                                    <button class="btn btn-danger"><a href="{{route('releases.list')}}">CANCELAR </a></button>
+                                    <button type="submit" class="btn btn-success">SALVAR</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-        <form action="{{route('release.editAction',$data->id)}}" method="POST">
-            @csrf<!--csrf toquem de segurnça padrao do laravel para envio de requisao-->
-            {{-- {{csrf_token()}} --}}
-            @method('PUT')<!--essa directiva é usada por causa do verdo put declarado na rota-->
-            <div class="row mt-4">
-                <div class="col-sm-6 offset-md-3">
-                    <label for="release_type">Tipo de Lançamento</label>
-                    <!--para carregar esse campo preenchido, verifica-se se existe um pet para edicao($data) se existir e for igual ao tipo de pet selecionado é carregado o campo preenchido com valor escolhido-->
-                    <select name="release_type" id="" class="form-select"  autofocus>
-                        <option value="">----Selecione----</option>
-                        @foreach($typeReleases as $typeRelease)
-                            <option value="{{$typeRelease}}" @if( isset($data) && $data -> release_type == $typeRelease) selected @endif>{{$typeRelease}}</option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 offset-md-3 mt-3">
-                    <label for="person">Pessoa</label>
-                    <input type="text" name="person" id="person" value="{{$data->person}}" class="form-control"  maxlength="30" >
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 offset-md-3 mt-3">
-                    <label for="amount">Valor</label>
-                    <input type="text" name="amount" id="amount" value="{{$data->amount}}" class="form-control" onkeyup="formatCoin();"  placeholder="R$" >
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 offset-md-3 mt-3">
-                    <label for="due_date">Data</label>
-                    <input type="date" name="due_date" id="due_date" value="{{$data->due_date}}" class="form-control" >
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 offset-md-3 mt-3">
-                    <label for="description">Descrição</label>
-                    <input type="text" name="description" id="description" value="{{$data->release_type}}" class="form-control"  maxlegth="30" >
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 offset-md-3 mt-4">
-                    <button class="btn btn-danger"><a href="{{route('releases.list')}}">CANCELAR </a></button>
-                    <button type="submit" class="btn btn-success">SALVAR</button>
-                </div>
-            </div>
-        </form>
     </div>
 @endsection
 
